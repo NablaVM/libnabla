8804e5c756141d89b74c1aa8a1cc44c218d11bf1
bosley <bosley117@gmail.com>
2020-06-03 09:10:09 -0400

    Endianess

--- lib/endian/endian.hpp
+++ lib/endian/endian.hpp
@@ -14,6 +14,8 @@
 #ifndef PORTABLE_ENDIAN_H__
 #define PORTABLE_ENDIAN_H__
 
+#include <stdint.h>
+
 #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
 
 #	define __WINDOWS__
